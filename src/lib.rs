ff5d878234d3bf671f3beab698e602d00482a8c6
Greg Unrein <greg@unre.in>
2024-09-28 08:08:07 -0700

    Move model computation into a library and add a test

---
+++ src/lib.rs
@@ -0,0 +1,178 @@
+use std::fmt::{Debug, Display, Formatter};
+
+/// Compute a linear model from a list of data
+pub fn compute_model(observations: Vec<Observation>) -> LinearModel {
+    // Convenience function for squaring a number
+    let square = |number| f32::powf(number, 2.0);
+
+    let count = observations.len() as f32;
+
+    // Compute the sum and average of the x values of the observations
+    let sum_of_x = observations.iter().map(|sample| sample.x).sum::<f32>();
+    let average_of_x = sum_of_x / count;
+
+    // Compute the sum and average of the y values of the observations
+    let sum_of_y = observations.iter().map(|sample| sample.y).sum::<f32>();
+    let average_of_y = sum_of_y / count;
+
+    // Calculate the Ordinary Least Squares linear regression for the training data
+    // https://en.wikipedia.org/wiki/Ordinary_least_squares
+    let numerator: f32 = observations
+        .iter()
+        .map(|observation| (observation.x - average_of_x) * (observation.y - average_of_y))
+        .sum();
+    let denominator: f32 = observations
+        .iter()
+        .map(|observation| square(observation.x - average_of_x))
+        .sum();
+    let beta = numerator / denominator;
+    let alpha = average_of_y - (beta * average_of_x);
+    let model = |x| x * beta + alpha;
+
+    // How well did the model do? Check using Root Mean Square Error.
+    // https://en.wikipedia.org/wiki/Root_mean_square_deviation
+    let sum_of_squares_of_residuals = observations
+        .iter()
+        .map(|observation| square(observation.y - model(observation.x)))
+        .sum::<f32>();
+    let root_mean_square_error = f32::sqrt(sum_of_squares_of_residuals / count);
+
+    let total_sum_of_squares = observations
+        .iter()
+        .map(|observation| square(observation.y - average_of_y))
+        .sum::<f32>();
+    let coefficient_of_determination = 1.0 - (sum_of_squares_of_residuals / total_sum_of_squares);
+
+    LinearModel {
+        alpha,
+        beta,
+        count,
+        sum_of_x,
+        average_of_x,
+        sum_of_y,
+        average_of_y,
+        root_mean_square_error,
+        coefficient_of_determination,
+    }
+}
+
+/// Represents the model and statistics computed from linear regression
+pub struct LinearModel {
+    // Model parameter values
+    pub alpha: f32,
+    pub beta: f32,
+
+    // Statistics
+    // The count of observations used to train the model
+    pub count: f32,
+    // The sum and average of the x values of the observations
+    pub sum_of_x: f32,
+    pub average_of_x: f32,
+    // The sum and average of the y values of the observations
+    pub sum_of_y: f32,
+    pub average_of_y: f32,
+    // The RMSE (Root Mean Square Error) of the model
+    pub root_mean_square_error: f32,
+    // The R² (Coefficient of Determination) of the model
+    pub coefficient_of_determination: f32,
+}
+
+impl LinearModel {
+    /// Use the model to predict the y value for a given x
+    #[allow(dead_code)]
+    fn predict(&self, x: f32) -> f32 {
+        x * self.beta + self.alpha
+    }
+}
+
+/// Pretty printing of a model and its statistics
+impl Display for LinearModel {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            r#"Model:     y_i = {} * x_i + {}
+⍺:         {}
+β:         {}
+Count:     {}
+x average: {} = {} / {}
+y average: {} = {} / {}
+RMSE:      {}
+R²:        {}"#,
+            self.beta,
+            self.alpha,
+            self.alpha,
+            self.beta,
+            self.count,
+            self.average_of_x,
+            self.sum_of_x,
+            self.count,
+            self.average_of_y,
+            self.sum_of_y,
+            self.count,
+            self.root_mean_square_error,
+            self.coefficient_of_determination
+        )
+    }
+}
+
+/// Represents a single observation or sample from an experiment
+pub struct Observation {
+    pub x: f32,
+    pub y: f32,
+}
+
+/// Prettier, more conventional printing of Observations
+impl Debug for Observation {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+/// Prettier, more conventional printing of Observations
+impl Display for Observation {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use float_cmp::assert_approx_eq;
+    use super::*;
+
+    #[test]
+    fn physics_ball_drop_experiment() {
+        let physics_drop_bounce_experiment = vec![
+            Observation { x: 10.0, y: 7.5 },
+            Observation { x: 10.0, y: 7.5 },
+            Observation { x: 10.0, y: 7.0 },
+            Observation { x: 12.0, y: 10.0 },
+            Observation { x: 12.0, y: 10.0 },
+            Observation { x: 12.0, y: 9.75 },
+            Observation { x: 14.0, y: 11.0 },
+            Observation { x: 14.0, y: 11.0 },
+            Observation { x: 14.0, y: 10.5 },
+            Observation { x: 16.0, y: 12.75 },
+            Observation { x: 16.0, y: 13.0 },
+            Observation { x: 16.0, y: 13.0 },
+            Observation { x: 18.0, y: 14.75 },
+            Observation { x: 18.0, y: 14.0 },
+            Observation { x: 18.0, y: 14.5 },
+            Observation { x: 20.0, y: 15.25 },
+            Observation { x: 20.0, y: 15.5 },
+            Observation { x: 20.0, y: 16.0 },
+        ];
+
+        let model = compute_model(physics_drop_bounce_experiment);
+
+        assert_approx_eq!(f32, -0.3452387, model.alpha);
+        assert_approx_eq!(f32, 0.8119048, model.beta);
+        assert_approx_eq!(f32, 18.0, model.count);
+        assert_approx_eq!(f32, 270.0, model.sum_of_x);
+        assert_approx_eq!(f32, 15.0, model.average_of_x);
+        assert_approx_eq!(f32, 213.0, model.sum_of_y);
+        assert_approx_eq!(f32, 11.833333, model.average_of_y);
+        assert_approx_eq!(f32, 0.41299996, model.root_mean_square_error);
+        assert_approx_eq!(f32, 0.9783022, model.coefficient_of_determination);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,8 @@
-use std::fmt::{Debug, Display, Formatter};
-
-/// Represents a single observation or sample from an experiment
-struct Observation {
-    x: f32,
-    y: f32,
-}
-
-/// Prettier, more conventional printing of Observations
-impl Debug for Observation {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "({}, {})", self.x, self.y)
-    }
-}
-
-impl Display for Observation {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "({}, {})", self.x, self.y)
-    }
-}
-
-fn square(number: f32) -> f32 {
-    f32::powf(number, 2.0)
-}
+use llm_from_scratch::{compute_model, Observation};
 
 fn main() {
     // Data from the drop bounce experiment. Units are in number of bricks.
-    let physics_drop_bounce_experiment = [
+    let physics_drop_bounce_experiment = vec![
         Observation { x: 10.0, y: 7.5 },
         Observation { x: 10.0, y: 7.5 },
         Observation { x: 10.0, y: 7.0 },
@@ -46,64 +23,5 @@ fn main() {
         Observation { x: 20.0, y: 16.0 },
     ];
 
-    let domain = 0..physics_drop_bounce_experiment.len();
-
-    println!("Data: {physics_drop_bounce_experiment:#?}");
-    let count = physics_drop_bounce_experiment.len() as f32;
-    let y_sum = physics_drop_bounce_experiment
-        .iter()
-        .map(|sample| sample.y)
-        .sum::<f32>();
-    let y_average = y_sum / count;
-    println!("y average: {y_average} = {y_sum} / {count}");
-    let x_sum = physics_drop_bounce_experiment
-        .iter()
-        .map(|sample| sample.x)
-        .sum::<f32>();
-    let x_average = x_sum / count;
-    println!("x average: {x_average} = {x_sum} / {count}");
-
-    // Calculate the Ordinary Least Squares linear regression for the training data
-    // https://en.wikipedia.org/wiki/Ordinary_least_squares
-    let indices: Vec<usize> = domain.clone().collect();
-    let numerator: f32 = indices
-        .iter()
-        .map(|i| {
-            (physics_drop_bounce_experiment.get(*i).unwrap().x - x_average)
-                * (physics_drop_bounce_experiment.get(*i).unwrap().y - y_average)
-        })
-        .sum();
-    let denominator: f32 = indices
-        .iter()
-        .map(|i| square(physics_drop_bounce_experiment.get(*i).unwrap().x - x_average))
-        .sum();
-    let beta = numerator / denominator;
-    let alpha = y_average - (beta * x_average);
-    let model = |x| x * beta + alpha;
-    println!("Model:     y_i = {beta} * x_i + {alpha}");
-
-    // How well did the model do? Check using Root Mean Square Error.
-    // https://en.wikipedia.org/wiki/Root_mean_square_deviation
-    let sum_of_squares_of_residuals = indices
-        .iter()
-        .map(|i| {
-            let observation = physics_drop_bounce_experiment.get(*i).unwrap();
-            let y_actual = observation.y;
-            let y_predicted = model(observation.x);
-            square(y_actual - y_predicted)
-        })
-        .sum::<f32>();
-    let root_mean_square_error = f32::sqrt(sum_of_squares_of_residuals / count);
-    println!("RMSE:      {root_mean_square_error}");
-
-    let total_sum_of_squares = indices
-        .iter()
-        .map(|i| {
-            let observation = physics_drop_bounce_experiment.get(*i).unwrap();
-            let y_actual = observation.y;
-            square(y_actual - y_average)
-        })
-        .sum::<f32>();
-    let coefficient_of_determination = 1.0 - (sum_of_squares_of_residuals / total_sum_of_squares);
-    println!("R²:        {coefficient_of_determination}");
+    println!("{}", compute_model(physics_drop_bounce_experiment));
 }

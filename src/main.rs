b59f65f1fffb76b17997bc6add20de5c6aec55fb
Greg Unrein <greg@unre.in>
2024-09-28 06:19:00 -0700

    Physics ball drop experiment

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,82 @@
-use rand::distributions::{Distribution, Uniform};
+use std::fmt::{Debug, Display, Formatter};
+
+/// Represents a single observation or sample from an experiment
+struct Observation {
+    x: f32,
+    y: f32,
+}
+
+/// Prettier, more conventional printing of Observations
+impl Debug for Observation {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+impl Display for Observation {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
 
 fn main() {
-    // let linear_function = |x, m, b| (m * x) + b;
+    // Data from the drop bounce experiment. Units are in number of bricks.
+    let physics_drop_bounce_experiment = [
+        Observation { x: 10.0, y: 7.5 },
+        Observation { x: 10.0, y: 7.5 },
+        Observation { x: 10.0, y: 7.0 },
+        Observation { x: 12.0, y: 10.0 },
+        Observation { x: 12.0, y: 10.0 },
+        Observation { x: 12.0, y: 9.75 },
+        Observation { x: 14.0, y: 11.0 },
+        Observation { x: 14.0, y: 11.0 },
+        Observation { x: 14.0, y: 10.5 },
+        Observation { x: 16.0, y: 12.75 },
+        Observation { x: 16.0, y: 13.0 },
+        Observation { x: 16.0, y: 13.0 },
+        Observation { x: 18.0, y: 14.75 },
+        Observation { x: 18.0, y: 14.0 },
+        Observation { x: 18.0, y: 14.5 },
+        Observation { x: 20.0, y: 15.25 },
+        Observation { x: 20.0, y: 15.5 },
+        Observation { x: 20.0, y: 16.0 },
+    ];
 
-    // This example is a bit funny because we're using a known linear function to generate
-    // the data to train the model (i.e. find the model parameters `m` and `b`) so it is a closed
-    // loop.
+    let domain = 0..physics_drop_bounce_experiment.len();
 
-
-    // Grab a random number generator (rng)
-    let mut random_number_generator = rand::thread_rng();
-
-    // Create a distribution of [-99,100) to draw random samples from
-    let distribution = Uniform::from(-99..100);
-    let domain = 0..201;
-    let y = 0..201;
-
-    // Generate 100 random samples from the distribution and store them in a vector (i.e. array)
-    let xs: Vec<i64> = domain.clone().collect();
-    let training_data: Vec<i64> = xs.iter().map(|_x| distribution.sample(&mut random_number_generator)).collect();
-
-    // Output the training data so we can see it
-    println!("Training data: {training_data:?}");
-    let y_sum = training_data.iter().sum::<i64>() as f64;
-    let y_count = training_data.len() as f64;
-    let y_average = y_sum / y_count;
-    println!("y average: {y_average} = {y_sum} / {y_count}");
-    let x_sum = xs.iter().sum::<i64>() as f64;
-    let x_count = xs.len() as f64;
-    let x_average = x_sum / x_count;
-    println!("x average: {x_average} = {x_sum} / {x_count}");
+    println!("Data: {physics_drop_bounce_experiment:#?}");
+    let count = physics_drop_bounce_experiment.len() as f32;
+    let y_sum = physics_drop_bounce_experiment
+        .iter()
+        .map(|sample| sample.y)
+        .sum::<f32>();
+    let y_average = y_sum / count;
+    println!("y average: {y_average} = {y_sum} / {count}");
+    let x_sum = physics_drop_bounce_experiment
+        .iter()
+        .map(|sample| sample.x)
+        .sum::<f32>();
+    let x_average = x_sum / count;
+    println!("x average: {x_average} = {x_sum} / {count}");
 
     // Calculate the Ordinary Least Squares linear regression for the training data
-    let indices: Vec<i64> = domain.clone().collect();
-    let numerator: f64 = indices.iter().map(|i| (*xs.get(*i as usize).unwrap() as f64 - x_average) * (*training_data.get(*i as usize).unwrap() as f64 - y_average)).sum();
-    let denominator: f64 = indices.iter().map(|i| f64::powf(*xs.get(*i as usize).unwrap() as f64 - x_average, 2.0_f64)).sum();
+    let indices: Vec<usize> = domain.clone().collect();
+    let numerator: f32 = indices
+        .iter()
+        .map(|i| {
+            (physics_drop_bounce_experiment.get(*i).unwrap().x - x_average)
+                * (physics_drop_bounce_experiment.get(*i).unwrap().y - y_average)
+        })
+        .sum();
+    let denominator: f32 = indices
+        .iter()
+        .map(|i| {
+            f32::powf(
+                physics_drop_bounce_experiment.get(*i).unwrap().x - x_average,
+                2.0,
+            )
+        })
+        .sum();
     let beta = numerator / denominator;
     let alpha = y_average - (beta * x_average);
     println!("Model: y_i = {beta} * x_i + {alpha}")
